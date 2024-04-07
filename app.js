ff16eeb98ffa1fdfb1cda27378528b2019635f62
Yonatan Dvir <64538919+yonatan-dvir@users.noreply.github.com>
2024-04-07 23:51:55 +0300

    Defined all 4 yargs commands - add, read, list, remove note

--- app.js
+++ app.js
@@ -1,3 +1,52 @@
+const yargs = require("yargs");
 const getNotes = require("./notes.js");
 
-console.log(getNotes());
+// Create add command
+yargs.command({
+  command: "add",
+  describe: "Add a note",
+  builder: {
+    title: {
+      describe: "Note title",
+      demandOption: true,
+      type: "string",
+    },
+    body: {
+      describe: "Note body",
+      demandOption: true,
+      type: "string",
+    },
+  },
+  handler: function () {
+    console.log("Adding a note...");
+  },
+});
+
+// Create remove command
+yargs.command({
+  command: "remove",
+  describe: "Remove a note",
+  handler: function () {
+    console.log("Removing a note...");
+  },
+});
+
+// Create list command
+yargs.command({
+  command: "list",
+  describe: "Listing out all notes",
+  handler: function () {
+    console.log("Listing out all notes...");
+  },
+});
+
+// Create read command
+yargs.command({
+  command: "read",
+  describe: "Read a note",
+  handler: function () {
+    console.log("Reading a note...");
+  },
+});
+
+yargs.parse();
